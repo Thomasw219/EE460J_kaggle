ee34d6f903d85200c5e1fdbe0d3d6a721676a7ee
thomasw219 <thomasw219@gmail.com>
2019-02-28 15:56:05 -0600

    more xgboost trials

--- xgboost_cross_validation.py
+++ xgboost_cross_validation.py
@@ -11,8 +11,8 @@ np.random.seed(42)
 
 X_train, y_train = get_train_data()
 X_test = get_test_data()
-depths = [6, 8, 10]
-n_estimators = [100, 300, 500]
+depths = [5]
+n_estimators = [300, 500, 700]
 
 for depth in depths:
     for n in n_estimators:
@@ -21,7 +21,7 @@ for depth in depths:
         grid_params = {
             'max_depth' : [depth],
             'learning_rate' : [0.05, 0.1, 0.5],
-            'lambda' : [0.001, 0.01, 0.1],
+            'lambda' : [0.001, 0.005, 0.01],
             'n_jobs' : [7],
             'n_estimators' : [n],
             'eval_metric' : ['auc']
@@ -35,4 +35,4 @@ for depth in depths:
         print(xgb_grid.best_params_)
         print("\n\n\n")
         y_test = xgb_grid.predict_proba(X_test)[:, 1:]
-        write_predictions(y_test, "xgb_classifier_" + str(depth) + "_" + str(n))
+        write_predictions(y_test, "xgb_classifier_" + str(depth) + "_" + str(n) + "_fine")
