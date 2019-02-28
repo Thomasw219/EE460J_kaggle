edefe0d6f7012d77a7a4070ad316679b5272cb2b
thomasw219 <thomasw219@gmail.com>
2019-02-28 00:47:12 -0600

    svm predictions

--- svm_cross_validation.py
+++ svm_cross_validation.py
@@ -5,6 +5,8 @@ import time
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 
+from extract_data import *
+
 np.random.seed(42)
 
 X_train, y_train = get_train_data()
@@ -12,10 +14,13 @@ X_test = get_test_data()
 
 model = SVC()
 grid_params = {
-    'C' : [0.25, 0.5, 1, 2]
+    'C' : [0.25, 0.5, 1, 2],
+    'probability' : [True],
+    'gamma' : ['scale']
 }
-xgb_grid = GridSearchCV(model, grid_params, cv=3, n_jobs=-1, scoring='roc_aoc', return_train_score='warn')
-xgb_grip.fit(X_train, np.ravel(y_train))
+xgb_grid = GridSearchCV(model, grid_params, cv=3, n_jobs=-1, scoring='roc_auc', return_train_score='warn')
+xgb_grid.fit(X_train, np.ravel(y_train))
+print("Best score: " + str(xgb_grid.best_score_))
 
 y_test = xgb_grid.predict_proba(X_test)[:, 1:]
 write_predictions(y_test, "svm_rbf")
