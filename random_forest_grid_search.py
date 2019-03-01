d7ebf1d1508b705c7ccb5703bb88edf00a8f3045
thomasw219 <thomasw219@gmail.com>
2019-02-28 18:44:39 -0600

    random forest additions

--- random_forest_grid_search.py
+++ random_forest_grid_search.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import time
-from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
 from extract_data import *
@@ -12,17 +12,15 @@ np.random.seed(42)
 X_train, y_train = get_train_data()
 X_test = get_test_data()
 
-model = SVC()
+model = RandomForestClassifier()
 grid_params = {
-    'C' : [5, 10, 20],
-    'probability' : [True],
-    'gamma' : ['scale', 'auto'],
-    'decision_function_shape' : ['ovo']
+    'n_estimators' : [350, 400, 450, 500, 550],
+    'criterion' : ['gini', 'entropy']
 }
-xgb_grid = GridSearchCV(model, grid_params, cv=3, n_jobs=-1, scoring='roc_auc', return_train_score='warn')
+xgb_grid = GridSearchCV(model, grid_params, cv=5, n_jobs=-1, scoring='roc_auc', return_train_score='warn')
 xgb_grid.fit(X_train, np.ravel(y_train))
 print("Best score: " + str(xgb_grid.best_score_))
 print(xgb_grid.best_params_)
 
 y_test = xgb_grid.predict_proba(X_test)[:, 1:]
-write_predictions(y_test, "svm_rbf_c" + str(xgb_grid.best_params_['C']))
+write_predictions(y_test, "random_forest_" + str(xgb_grid.best_params_['n_estimators']))
